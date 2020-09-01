5d20fc23f441d20180bfd7e92fcacb0925257c5b
teoyi <yipengteo@yahoo.com.sg>
2020-08-31 19:52:35 -0700

    finished UI for the game, additioanl things to be added when needed

--- scripts/script2.js
+++ scripts/script2.js
@@ -1,55 +1,58 @@
-// Backend for rock paper scissors game
+// // Backend for rock paper scissors game
 
-// Randomizing computer's hand 
-function computerPlay() {
-    let choice = ["rock", "paper", "scissors"];
-    let random = Math.floor(Math.random() * choice.length);
-    return result = choice[random]
-}
+// // Randomizing computer's hand 
+// function computerPlay() {
+//     let choice = ["rock", "paper", "scissors"];
+//     let random = Math.floor(Math.random() * choice.length);
+//     return result = choice[random]
+// }
 
-// Game Logic 
-function playRound(playerSelection, computerSelection) { 
+// // Game Logic 
+// function playRound(playerSelection, computerSelection) { 
     
-    console.log(`You played: ${playerSelection}`);
-    console.log(`The computer played: ${computerSelection}`);
-    if (playerSelection === computerSelection) {
-        return "It's a Tie!"
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        return "You Lose! Paper beats Rock!"
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "You Win! Rock beats scissors!"
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "You Win! Scissors beats paper!"
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return "You Lose! Rock beats scissors"
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You Win! Paper beats rock!"
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return "You Lose! Scissors beats paper!"
-    }
-}
+//     console.log(`You played: ${playerSelection}`);
+//     console.log(`The computer played: ${computerSelection}`);
+//     if (playerSelection === computerSelection) {
+//         return "It's a Tie!"
+//     } else if (playerSelection === "rock" && computerSelection === "paper") {
+//         return "You Lose! Paper beats Rock!"
+//     } else if (playerSelection === "rock" && computerSelection === "scissors") {
+//         return "You Win! Rock beats scissors!"
+//     } else if (playerSelection === "scissors" && computerSelection === "paper") {
+//         return "You Win! Scissors beats paper!"
+//     } else if (playerSelection === "scissors" && computerSelection === "rock") {
+//         return "You Lose! Rock beats scissors"
+//     } else if (playerSelection === "paper" && computerSelection === "rock") {
+//         return "You Win! Paper beats rock!"
+//     } else if (playerSelection === "paper" && computerSelection === "scissors") {
+//         return "You Lose! Scissors beats paper!"
+//     }
+// }
 
-// Actual Game 
-function game() {
-    //for (let step = 0; step < 5; step++){
+// // Actual Game 
+// function game() {
+//     //for (let step = 0; step < 5; step++){
 
-        // Setting variables of player's and computer's hand
-        let computerSelection = computerPlay();
-        let playerSelection = prompt("What hand would you like to play?");
-        playerSelection = playerSelection.toLowerCase();
+//         // Setting variables of player's and computer's hand
+//         let computerSelection = computerPlay();
+//         let playerSelection = prompt("What hand would you like to play?");
+//         playerSelection = playerSelection.toLowerCase();
 
-        let valid = false;
-        while (!valid) {
-            if (playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors") {
-                valid = true; 
-            } else {
-                playerSelection = prompt("Sorry, that is not an option! Please choose from rock, paper or scissors!");
-            }
-        }
-        console.log(playerSelection);
-        //Displaying results 
-        console.log(playRound(playerSelection, computerSelection));
-    //}
-}
+//         let valid = false;
+//         while (!valid) {
+//             if (playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors") {
+//                 valid = true; 
+//             } else {
+//                 playerSelection = prompt("Sorry, that is not an option! Please choose from rock, paper or scissors!");
+//             }
+//         }
+//         console.log(playerSelection);
+//         //Displaying results 
+//         console.log(playRound(playerSelection, computerSelection));
+//     //}
+// }
 
-game();
+// game();
+
+let pscore = 0;
+let cscore = 0;
